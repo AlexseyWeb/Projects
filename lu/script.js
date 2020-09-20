3b460e6c628d89ead8b98ddb011a42ae47931e56
AlexseyWeb <56154951+AlexseyWeb@users.noreply.github.com>
2020-09-20 17:16:44 +0300

    Update script.js

--- lu/script.js
+++ lu/script.js
@@ -1,17 +1,18 @@
 "use strict"
 
-//Скрипт начнет работать когда загрузится все узлы 
+//Проверка зарузки дом контента 
 window.addEventListener('DOMContentLoaded', () =>{
    const btnCall = document.querySelector('.btn')
    let btnRename = () => btnCall.textContent = '+7 (930) 840-40-04'
    const btnUp = document.querySelector('#upbutton')
 
+   //Добавил обработчик события если указатель мыши пройдет
     btnCall.addEventListener('mouseover', (e) =>{
         e.preventDefault()
         btnRename()
     })
 
-    //Кнопка навверх
+   
     let smoothJumpUp = () => {
         if(document.body.scrollTop > 0 || document.documentElement.scrollTop>0){
             window.scrollBy(0, -200)
@@ -26,6 +27,7 @@ window.addEventListener('DOMContentLoaded', () =>{
             document.querySelector('#upbutton').style.display = 'none'
         }
     }
+    //Обработчик события на нажатие стрелки 
     btnUp.addEventListener('click', () =>{
         smoothJumpUp()
     })
