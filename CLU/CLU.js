dbe52bc7db7bf482da8fd2786f0e6fe1cc7fd993
Alexsey <56154951+AlexseyWeb@users.noreply.github.com>
2020-10-06 23:09:40 +0300

    a small function CLU
    a small function for  working in console of DevTools

---
+++ CLU/CLU.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/* Функция для работы с консоли разработчика */
+
+
+function clu(string = 'not text', css = 'color:orange; text-shadow: 10px 10px 10px white; font-size:20px') {
+    console.log(`%c ${string}`, `${css};`)
+}
+
+const second = 'font-size:18px; background:rgba(33,44,88,0.5); color:orange;border-radius:40px 10px 40px'
+
+clu('Welcome to Console DevTools!')
+clu('First argument a text. A second argument css ', second)
+clu(clu)
+
+
+export default clu
